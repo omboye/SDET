4796659b2890db27359c8a92fefc118122afedf2
omboye <omboyeeri82@gmail.com>
2020-02-18 14:32:53 +0100

    Add files via upload

---
+++ InstallTestEditor/logs.ps1
@@ -0,0 +1,12 @@
+﻿#Handling log errors we use the try catch then store the logs in the new.txt
+
+$SearchLog  = “C:\Users\Omboye\Desktop\logs\new.txt”
+Try{
+  Get-ChildItem -Path "~\Documents\*.g" -Recurse -ErrorAction Stop -ErrorVariable SearchError
+}
+Catch{
+  Add-Content -Path $SearchLog -Value "$(Get-Date -Format dd-MM-yy-hh_mm_ss) Files not found returned error: $SearchError"
+Write-Host "$SearchLog"
+}
+Write-Host "$SearchLog"
+
